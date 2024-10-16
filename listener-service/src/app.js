cfa94446943d7ff48acc8986b016af10193befe5
Aman Gusain <73630808+amangusain302@users.noreply.github.com>
2024-10-16 14:12:22 +0530

    changes in docker

--- listener-service/src/app.js
+++ listener-service/src/app.js
@@ -3,4 +3,8 @@ require('dotenv').config();
 
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send("Listener-service is running")
+})
+
 module.exports = app;
--- receiver-service/src/app.js
+++ receiver-service/src/app.js
@@ -6,7 +6,9 @@ require('dotenv').config();
 const app = express();
 app.use(bodyParser.json());
 
-
+app.get("/", (req, res) => {
+    res.send("Reciver-service is running")
+})
 app.use('/receiver', receiverRoutes);
 
 module.exports = app;
